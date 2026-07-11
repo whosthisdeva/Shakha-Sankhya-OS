25946f993ffb50bd436f6c7c9b1a88d0a3e1e4b8
whosthisdeva <mailtoevagarg@gmail.com>
2026-07-10 18:59:05 -0700

    initialize firebase on app startup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./hooks/useAuth";
 import "./styles.css";
+import "./firebase";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
